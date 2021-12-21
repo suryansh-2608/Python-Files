5a0300b2c029b73e4eb7dd1e55cb319390fa87a3
Suryansh Bhardwaj <84323661+suryansh-2608@users.noreply.github.com>
2021-12-21 20:48:21 +0530

    v3.0

--- pythonProject/Module3.py
+++ pythonProject/Module3.py
@@ -1,2 +1,62 @@
-ls = ["Ashish", "hello", 45, 12.3, 78421315]
-print(ls)
+# LISTS
+# ls = ["Ashish", "Avinash", "Ajay", "Ramesh", "Rajesh", "Suryansh"]
+# #
+# # # print(ls[1:4])
+# # # print(ls[-1])
+# # # ls[0] = "Aashish"
+# # # print(ls)
+# # # ls.append("Ajay")
+# # # print(ls)
+# # # ls.insert(3, "Akhilesh")
+# # # print(ls)
+# # # del(ls[3])
+# # # print(ls)
+# # # print(ls[2:5])
+# # print(ls[:3])
+# for i in ls:
+#     print(i)
+# ls = [1, 25, 4561, 236, 185496851, 131, 3298, 2154, 516, 1654165, 85468461, 1216, 8548641651, 56165, 541641, 61]
+# maximum = -1
+# for i in ls:
+#     if maximum < i:
+#         maximum = i
+# print(maximum)
+# print(max(ls))
+# for i in ls:
+#     print(ls)
+# print(len(ls))
+############################################################################
+# FUNCTIONS
+# lt = [1, 25, 4561, 236, 185496851, 131, 3298, 2154, 516, 1654165, 85468461, 1216, 8548641651, 56165, 541641, 61]
+# def length(ls):
+#     count = 0
+#     for i in ls:
+#         count += 1
+#     print(count)
+# def mini(ls):
+#     minimum = 123333333333333333333333333333
+#     for i in ls:
+#         if i < minimum:
+#             minimum = i
+#     print(minimum)
+#
+#
+# lt = [1, 25, 4561, 236, 185496851, 131, 3298, 2154, 516, 1654165, 85468461, 1216, 8548641651, 56165, 541641, 61]
+# mini(lt)
+# lt = [1, 25, 4561, 236, 185496851, 131, 3298, 2154, 516, 1654165, 85468461, 1216, 8548641651, 56165, 541641, 61]
+#
+#
+# def sum(ls):
+#     suum = 0
+#     for i in lt:
+#         suum += i
+#     print(suum)
+#
+# sum(lt)
+# def power(a, b):
+#     return (a**b)
+#
+#
+# print(power(2, 10))
+
+
