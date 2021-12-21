1468071b2cde579d9fb1124250899fd1bc6303b0
suryansh-2608 <suryanshbhardwaj47@gmail.com>
2021-12-21 19:30:33 +0530

    v1.0

---
+++ pythonProject/Module1And2.py
@@ -0,0 +1,78 @@
+# print("Hello World")
+# print(type(15))
+# var = "This is my car"
+# print(var)
+# print("MY name is")
+# num = int(input("Enter a number: "))
+# print(num)
+# print(type(num))
+# a = 10
+# b = 5
+# x = a + b
+# print(x)
+# money = 100
+# expense = int(input("Enter your expense: "))
+# remainder = money - expense
+# chocolate = 10
+# if (remainder > 0):
+#     if(remainder >= chocolate):
+#         print("Yes, we can buy chocolate.")
+#     else:
+#         print("You can't buy a chocolate")
+# else:
+#     print("You have gone over budget")
+# for n in "banana":
+#     print(n)
+# for n in range(1,10):
+#     print(n)
+# for n in range(1,10,2):
+#     print(n)
+# for n in range(-18, 10):
+#     print(n)
+# sum = 0
+# n = int(input("Please Enter the Range: "))
+# for i in range(n):
+#     sum += i
+# print(sum)
+# sum = 0
+# n = int(input("Please enter the range: "))
+# i=0
+# while i<= n:
+#     sum += i
+#     i+=1
+# print(sum)
+# sum = 0
+# for i in range(1,6):
+#     sum += i**3
+# print(sum)
+# sum = 0
+# n = int(input("Please Enter the range: "))
+# i = 1
+# while i <= n:
+#     sum+=i**3
+#     i+=1
+# print(sum)
+# for i in range(1,5):
+#     if(i==5):
+#         break
+#     print(i)
+# i = 0
+# while i <= 5:
+#     if (i == 3):
+#         break
+#     i += 1
+#     print(i)
+# for i in range(1,11):
+#     if i==5:
+#         continue
+#     print(i)
+# i = 0
+# while i <= 10:
+#     i += 1
+#     if i == 5 or i == 8:
+#         continue
+#     print(i)
+# for i in range(1, 5):
+#     for j in range(1, 5):
+#         print(i+j, end= " ")
+#     print("\n")
