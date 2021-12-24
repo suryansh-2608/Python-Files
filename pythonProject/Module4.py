ee8cb5d7bb9dc03e849649d2cdc5ccdd7a5b9da4
Suryansh Bhardwaj <84323661+suryansh-2608@users.noreply.github.com>
2021-12-25 03:04:52 +0530

    Update Module4.py

--- pythonProject/Module4.py
+++ pythonProject/Module4.py
@@ -18,3 +18,19 @@
 # print(s[14:96])
 # print(s[20:])
 # print(s[:99])
+# string = "Python Lover"
+#
+# for i in range(len(string)):
+#     if string[i] == "L":
+#         print(i)
+#
+# char = "o"
+# print(string.find(char))
+# print(string.rfind(char))
+# TheAvengers = "Hulk Iron Man Captain America Thor Black Widow Wanda Vision Hawkeye Captain Marvel Doctor Strange Winter Soldier Spider-Man Black Panther Ant-Man Rocket Star Lord Wong Avengers"
+# print(TheAvengers.upper())
+# print(TheAvengers.lower())
+# print(TheAvengers.capitalize())
+# print(TheAvengers.split())
+# print(TheAvengers.count("n"))
+# print(TheAvengers.zfill(200))
