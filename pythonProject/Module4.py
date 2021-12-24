e30a6b1fff55101753f81a010709147e3e232cbc
Suryansh Bhardwaj <84323661+suryansh-2608@users.noreply.github.com>
2021-12-25 02:47:25 +0530

    Update Module4.py

---
+++ pythonProject/Module4.py
@@ -0,0 +1,20 @@
+# hi = "hello "
+# print(hi[2])
+# name = input("Enter your name: ")
+# print(hi + name)
+# hi += name
+# print(hi)
+# hi *= 5
+# print(hi)
+# hi += "World"
+# print(hi)
+# var = "r" + hi
+# print(var)
+# ls1 = [1, 2, 3, 4, 5, 6]
+# ls2 = [7, 8, 9, 10]
+# print(ls1 + ls2)
+# s = "Hulk Iron Man Captain America Thor Black Widow Wanda Vision Hawkeye Captain Marvel Doctor Strange Winter Soldier Spider-Man Black Panther Ant-Man Rocket Star Lord Wong Avengers"
+# print(s[:])
+# print(s[14:96])
+# print(s[20:])
+# print(s[:99])
