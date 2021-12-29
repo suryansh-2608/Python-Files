d9485476a79e13837b31d1783b2f1819cb46f7f4
Suryansh Bhardwaj <84323661+suryansh-2608@users.noreply.github.com>
2021-12-29 20:30:36 +0530

    Create Module6.py

---
+++ pythonProject/Module6.py
@@ -0,0 +1,10 @@
+import numpy as np
+# a = np.array([1, 2, 3])
+# print(a)
+# print(type(a))
+# print(a.shape)
+# print(a[0], a[1], a[2])
+b = np.array([[1, 2, 3], [4, 5, 6]])
+print(b)
+print(type(b))
+print(b.shape)
