fe70bd03a628493f7b5ec4a60fe09fa183c12679
Suryansh Bhardwaj <84323661+suryansh-2608@users.noreply.github.com>
2022-01-02 16:34:25 +0530

    Update web.py

--- pythonProject/web.py
+++ pythonProject/web.py
@@ -1,6 +1,6 @@
 import requests
 import bs4
-
+import lxml.etree as xml
 url = "https://github.com/requests/requests"
 
 response = requests.get(url)
@@ -9,11 +9,11 @@ response = requests.get(url)
 
 web_page = bs4.BeautifulSoup(response.text, "lxml")
 
-# print(web_page.head)
-# print(web_page.head.title)
-# print(web_page.head.title.text)
+print(web_page.head)
+print(web_page.head.title)
+print(web_page.head.title.text)
 
-# print(web_page.body)
+print(web_page.body)
 
 sub_page = web_page.find_all(name="ul")
 print(sub_page[3])
